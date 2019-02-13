eded4ec7b5bc55e512f698bef423d75ba7989068
Miguel Gonzalez <miguelg71921@gmail.com>
2019-02-13 13:18:11 -0800

    New Logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './KawaiiMartv2.png';
 import './App.css';
 
 class App extends Component {
@@ -8,9 +8,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
