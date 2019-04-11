87282d2b710afc4020a6122255c9bfb2a83aeee3
andres-cortes <andres.cortes@sjsu.edu>
2019-04-11 14:33:31 -0700

    Started home page (doesnt work)

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-
+import './home.css'
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                Home Component
+            <div className = "home cover-container d-flex h-100 p-3 mx-auto flex-column">
+                <header class= "masthead mb-auto">
+                  <h3 class = "masthead-brand"> COVER </h3>
+                </header>
+                <main role = "main" class= "inner cover">
+                  <h1 class="cover-heading">Home page</h1>
+                  <p class = "lead">
+                    "test test test test test test test."
+                  </p>
+                </main>
             </div>
         );
     }
