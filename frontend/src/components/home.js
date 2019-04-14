3d0c4148f18da2718b722d72896ef0e83e84d4ff
andres-cortes <andres.cortes@sjsu.edu>
2019-04-14 15:57:23 -0700

    Template for Home page

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -3,15 +3,34 @@ import './home.css'
 export default class Home extends Component {
     render() {
         return (
-            <div className = "home cover-container d-flex h-100 p-3 mx-auto flex-column">
-                <header class= "masthead mb-auto">
-                  <h3 class = "masthead-brand"> COVER </h3>
+            <div className = "home">
+                <header class= "cover">
+                  <h3 class = "welcome"> Welcome to KawaiiMart! </h3>
                 </header>
-                <main role = "main" class= "inner cover">
-                  <h1 class="cover-heading">Home page</h1>
-                  <p class = "lead">
-                    "test test test test test test test."
-                  </p>
+                <main class= "inner cover">
+                  <h4 class="cover-heading">See today's featured items</h4>
+                  <div class= "featured">
+                      <p class = "item">
+                      "Featured: __ Cereal"
+                      </p>
+                      <p class = "item">
+                      "Featured: __ Snack"
+                      </p>
+                      <p class = "item">
+                      "Featured: __ Brand Veggies"
+                      </p>
+                  </div>
+                  <div class= "featured">
+                      <p class = "item">
+                      "Featured: __ Soda"
+                      </p>
+                      <p class = "item">
+                      "Featured: __ Chips"
+                      </p>
+                      <p class = "item">
+                      "Featured: __ Pastries"
+                      </p>
+                  </div>
                 </main>
             </div>
         );
