cbc0ed69d1f986c9e09af4f717ea123794e51f11
miguelg719 <miguelg71921@gmail.com>
2019-04-30 17:24:05 -0700

    Cart functional

--- routes/cart.js
+++ routes/cart.js
@@ -14,45 +14,48 @@ router.post('/', function(req, res) {
       product: req.body.product,
       quantity: req.body.quantity,
     };
-    const uId = req.body.id;
+    const uId = req.body._id;
     let uCart = false;
 
     User.findOne({
-      id: uId,
+      _id: uId,
     }).then(user => {
       uCart = user.cart;
-      if(uCart) {
-        Cart.findOne({
-          user: uId,
-        }).then(cart => {
-          if(cart) {
-            let products = cart.items.map((item) => item.product + '');
-            if (products.includes(item.product)) {
-              Cart.findOneAndUpdate({
-                user: uId,
-                items: {
-                  $elemMatch: { product: item.product }
-                }
-              },
-                {
-                  $inc: { 'items.$.quantity': item.quantity }
-                })
-                .exec()
-                .then(() => res.end());
-            } else {
-              cart.items.push(item);
-              cart.save().then(() => res.end());
-            }
+      if(!user) {
+          return res.status(404).json('User was not found');
+      }
+      Cart.findOne({
+        user: uId,
+      }).then(cart => {
+        if(cart) {
+          let products = cart.items.map((item) => item.product + '');
+          if (products.includes(item.product)) {
+            Cart.findOneAndUpdate({
+              user: uId,
+              items: {
+                $elemMatch: { product: item.product }
+              }
+            },
+              {
+                $inc: { 'items.$.quantity': item.quantity }
+              })
+              .exec()
+              .then(() => res.end('Quantity increased'));
+          } else {
+            cart.items.push(item);
+            cart.save().then(() => res.end('Items pushed'));
           }
-        });
-      } else {
-        user.cart = true;
-        Cart.create({
-          user: uId,
-          items: [item]
-        })
-          .then(() => res.end());
-      }});
+        } else {
+          user.cart = true;
+          Cart.create({
+            user: uId,
+            items: [item]
+          })
+            .then(() => res.end('New Cart Created'));
+        }
+    });
+  });
+
 });
 
 router.get('/', function(req, res) {
@@ -70,11 +73,12 @@ router.get('/', function(req, res) {
 
 router.put('/', function(req, res) {
 
-  Cart.findById(req.body.cartId)
+  Cart.findById(req.body.id)
     .then((cart) => {
       cart.items = cart.items.filter((item) => item._id != req.body.itemId);
       cart.save(() => res.end());
     });
+
 });
 
 router.delete('/', function(req, res) {
@@ -84,4 +88,4 @@ router.delete('/', function(req, res) {
     .catch((err) => res.send(err));
 });
 
-module.exports = router
+module.exports = router;
